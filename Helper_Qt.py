f579aa0f71d931dd8984160a411ebf1d7aeef774
Gri-Gri <gri_simonov@mail.ru>
2019-01-30 19:33:05 +0300

    Made another stuff with adding_tasks

--- Helper_Qt.py
+++ Helper_Qt.py
@@ -1,24 +1,13 @@
 import sys
-from contact_with_base import get_tasks, add_task
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QInputDialog
+from contact_with_base import DatabaseClass
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QInputDialog, QLineEdit, QRadioButton, QCheckBox
 from main_window_ui import Ui_MainWindow
 from dialog_to_choose_mode_in_testing_ui import Ui_Dialog as Ui_choosing_dialog
 from dialog_adding_task_ui import Ui_Dialog as Ui_adding_task
 from warning_dialog_ui import Ui_Dialog as Ui_warning_dialog
-
-
-def open_warning_dialog(parent, title, label_text):
-    class WarningDialog(QDialog, Ui_warning_dialog):
-        def __init__(self, parent, title, label_text):
-            super().__init__(parent)
-            self.setupUi(self)
-            self.setTitle(title)
-            self.label.setText(label_text)
-            self.label.resize(self.label.sizeHint())
-            self.pushButton.clicked.connect(self.close)
-            
-    dialog = WarningDialog(parent, title, label_text)
-    dialog.show()
+from dialog_testing_ui import Ui_Dialog as Ui_dialog_testing
+from random import shuffle
+from itertools import zip_longest
 
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
@@ -26,34 +15,20 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.btn_test.clicked.connect(self.go_to_testing)
-        '''self.already_test_exists = False'''
-        
+
     def go_to_testing(self):
         test_mode_choosing_dialog = DialogOnChoosingTestingMode(self)
         test_mode_choosing_dialog.show()
-        
-    '''
-    def go_to_testing(self):
-        if not self.already_test_exists:
-            test_mode_choosing_dialog = DialogOnChoosingTestingMode(self)
-            test_mode_choosing_dialog.show()
-            self.already_test_exists = True
-    '''
-    
+
+
 class DialogOnChoosingTestingMode(QDialog, Ui_choosing_dialog):
     def __init__(self, parent):
         self.parent = parent
         super().__init__(parent)
         self.setupUi(self)
         self.btn_get_test.clicked.connect(self.start_test)
-        self.btn_add_test.clicked.conncet(self.add_test)
+        self.btn_add_test.clicked.connect(self.add_test)
 
-    '''
-    def closeEvent(self, event):
-        self.parent.already_test_exists = False
-        super().closeEvent()
-    '''
-    
     def start_test(self):
         pass
 
@@ -76,33 +51,60 @@ class DialogOnAddingTask(QDialog, Ui_adding_task):
         self.variants = []
         self.mode = REGIMS["Выбор одного из предложенных вариантов"]
         self.numbers = []
+        self.switch_to_multi_variant_mode()
 
     def choose_task_type(self):
-        open_warning_dialog(self, 'Выбирайте с умом',
-                                "Учтите уже введщённые данные по заданию" +
-                            " при выборе типа задания, пожалуйста!")
+        foo = self.mode
         i, okBtnPressed = QInputDialog.getItem(self,
                                                "Тип задания",
+                                               "Выбирайте с умом!\n\nУчтите уже введённые данные по вопросу\n" +
+                                               "при выборе типа задания, пожалуйста!\n\n" +
                                                "Выберите тип задания",
                                                REGIMS.keys(), 1, False)
         if okBtnPressed:
             self.mode = REGIMS[i]
+        if self.mode == 1:
+            self.switch_to_one_answer_mode()
+        elif foo == 1 and self.mode != 1:
+            self.switch_to_multi_variant_mode()
         self.rewrite()
-            
+
+    def switch_to_one_answer_mode(self):
+        self.btn_add_right_variant_number.setVisible(False)
+        self.label_4.setVisible(False)
+        self.label_right_variants_numbers.setVisible(False)
+        self.btn_add_variant.setVisible(False)
+        self.label_3.setVisible(False)
+        self.plainTextEdit_2.setVisible(False)
+        self.label_5.setVisible(True)
+        self.lineEdit.setVisible(True)
+        self.resize(328, 471)
+
+    def switch_to_multi_variant_mode(self):
+        self.btn_add_right_variant_number.setVisible(True)
+        self.label_4.setVisible(True)
+        self.label_right_variants_numbers.setVisible(True)
+        self.btn_add_variant.setVisible(True)
+        self.label_3.setVisible(True)
+        self.plainTextEdit_2.setVisible(True)
+        self.label_5.setVisible(False)
+        self.lineEdit.setVisible(False)
+        self.resize(652, 471)
+
     def add_variant(self):
         if self.mode == 1:
             open_warning_dialog(self, 'Неверный тип задания',
                                 "Выберите другой тип задания")
             return None
         if len(self.variants) == 4:
-            open_warning_dialog(self, 'Слишком много вариантов',
-                                "Введённый далее вариант перезапишет последний!")
-        i, okBtnPressed = QInputDialog.getText(
-            self, "Добавление варианта ответа", "Введите вариант ответа")
-        if okBtnPressed:
-            if len(self.variants) == 4:
+            i, okBtnPressed = QInputDialog.getText(
+                self, "Слишком много вариантов", "Введённый далее вариант перезапишет последний!")
+            if okBtnPressed:
                 self.variants[-1] = i
-            else:
+        else:
+            i, okBtnPressed = QInputDialog.getText(
+                self, "Добавление варианта ответа", "Введите вариант ответа")
+            if okBtnPressed:
                 self.variants.append(i)
         self.rewrite()
 
@@ -112,42 +114,128 @@ class DialogOnAddingTask(QDialog, Ui_adding_task):
                                 "Выберите другой тип задания")
             return None
         if self.mode == 2 and len(self.numbers) == 1:
-            open_warning_dialog(self, 'Слишком много цифр',
-                                "виду выбранного вами типа задания," +
-                                " введённый далее номер варианта ответа" +
-                                " перезапишет уже имеющийся")
-        i, okBtnPressed = QInputDialog.getInt(
-            self, "Правильный ответ", "Введите номер правильного ответа",
-            1, 1, len(self.variants), 1)
+            i, okBtnPressed = QInputDialog.getInt(
+                self, "Правильный ответ", "Ввиду выбранного Вами типа задания,\n" +
+                                          "введённый далее Вами номер правильного ответа\nперезапишет" +
+                " уже существующий",
+                1, 1, len(self.variants), 1)
+        else:
+            i, okBtnPressed = QInputDialog.getInt(
+                self, "Правильный ответ", "Введите номер правильного ответа",
+                1, 1, len(self.variants), 1)
         if okBtnPressed:
             if self.mode == 2:
                 self.numbers = [i]
             else:
                 self.numbers.append(i)
+        self.numbers = sorted(list(set(self.numbers)))
         self.rewrite()
 
     def rewrite(self):
         self.label_task_type.setText(REGIMS_BACKWARDS[self.mode])
         self.label_task_type.resize(self.label_task_type.sizeHint())
-        self.plainTextEdit_2.setText('\n'.join(self.variants))
-        self.label_right_variants_numbers.setText(self.numbers)
+        self.plainTextEdit_2.setPlainText('\n'.join(self.variants))
+        self.label_right_variants_numbers.setText(", ".join(map(str, self.numbers)))
         self.label_right_variants_numbers.resize(self.label_right_variants_numbers.sizeHint())
-        
+
     def submit(self):
-        add_task(typ=self.mode, task_text=self.plainTextEdit.text(), variants=self.variants,
-                 right_nums=self.numbers)
+        if self.mode != 1:
+            database.add_task({"type": self.mode, 'task_text': self.plainTextEdit.toPlainText(),
+                               'variants': self.variants, 'right_nums': self.numbers})
+        else:
+            database.add_task({"type": self.mode, 'task_text': self.plainTextEdit.toPlainText(),
+                               'variants': self.lineEdit.text(), 'right_nums': None})
         open_warning_dialog(self, "Успешно!", "Вопрос добавлен в реестр!")
 
     def my_exit(self):
-        super().closeEvent()
+        super().close()
+
+
+class DialogOnTesting(QDialog, Ui_dialog_testing):
+    def __init__(self, parent):
+        self.parent = parent
+        super().__init__()
+        self.setupUi(self)
+        self.btn_answer.clicked.connect(self.answer)
+        self.counter_right = 0
+        self.counter_all = 0
+        self.n = 10
+        self.data = database.data
+        self.right_answer = None
+        self.answer_inputs = []
+        self.data = iter(shuffle(self.data)[:self.n])
+        self.rewrite()
+        self.now_mode = -1
+
+    def answer(self):
+        if self.now_mode == 1:
+            
+
+    def rewrite(self):
+        self.progressBar.setValue(self.counter_all * (100 // self.n))
+        self.lcd_all.display(self.counter_all)
+        self.lcd_right.display(self.counter_right)
+        working_task = next(self.data)
+        mode = working_task['type']
+        task_text = working_task['task_text']
+        self.plainTextEdit.setPlainText(task_text)
+        if mode == 1:
+            self.answer_inputs = [QLineEdit(self)]
+            self.answer_inputs[0].move(30, 200)
+            self.right_answer = working_task['variants'][0]
+        else:
+            self.answer_inputs = []
+            variants = working_task['variants']
+            right_nums = working_task['right_nums']
+            bar = len(variants)
+            for x, y, text, indexx in zip_longest([30]*bar, range(200, 201+30*bar), variants, enumerate(1)):
+                is_right = True if indexx in right_nums else False
+                btn = MyQRadioButton(self, is_right) if mode == 2 else MyQCheckBox(self, is_right)
+                if indexx == 1:
+                    btn.setChecked(True)
+                btn.move(x, y)
+                btn.setText(text)
+                self.answer_inputs.append(btn)
+        self.now_mode = mode
+
+
+class MyQRadioButton(QRadioButton):
+    def __init__(self, parent, is_right):
+        super().__init__(parent)
+        self.is_right = is_right
+
+
+class MyQCheckBox(QCheckBox):
+    def __init__(self, parent, is_right):
+        super().__init__(parent)
+        self.is_right = is_right
+
+
+def open_warning_dialog(parent, title, label_text):
+    class WarningDialog(QDialog, Ui_warning_dialog):
+        def __init__(self, parent, title, label_text):
+            super().__init__(parent)
+            self.setupUi(self)
+            self.setWindowTitle(title)
+            self.label.setText(label_text)
+            self.label.resize(self.label.sizeHint())
+            self.pushButton.clicked.connect(self.close)
+            self.resize(500, 100)
+
+    dialog = WarningDialog(parent, title, label_text)
+    dialog.show()
 
 
 REGIMS = {"Ввод пользовательского ответа": 1,
           "Выбор одного из предложенных вариантов": 2,
           "Выбор нескольких из предложенных вариантов": 3}
-
-REGIMS_BACKWARDS = {i[1]:i[0] for i in REGIMS.items()}
-
+REGIMS_BACKWARDS = {i[1]: i[0] for i in REGIMS.items()}
 
 if __name__ == '__main__':
+    database = DatabaseClass()
     app = QApplication(sys.argv)
+    window = MyMainWindow()
+    window.show()
+    my_exit_code = app.exec()
+    database.end()
+    sys.exit(my_exit_code)
